2ac0940e32104962be3cb7f464f6addc730bc1ca
guyingd <2739218253@qq.com>
2022-10-23 12:45:23 +0800

    deploy

--- config/announcementBar.js
+++ config/announcementBar.js
@@ -1,6 +1,6 @@
 module.exports = {
   id: 'announcementBar-02', // Increment on change
   content:
-    "This is not the official website of <a target='_blank' rel='noopener noreferrer' href='https://purrnelopescountryclub.com/''>Purrnelope's Country Club</a>.  Read more <a rel='noopener noreferrer' href='/about''>about PCC Archive</a>.",
+    "本文档正在改造建设中，如果你有更好的建议请加入 <a target='_blank' rel='noopener noreferrer' href='https://jq.qq.com/?_wv=1027&k=8u9L8t20''>孤影的QQ群</a>.  了解更多信息 <a rel='noopener noreferrer' href='/about''>关于文档</a>.",
   isCloseable: false,
 }
